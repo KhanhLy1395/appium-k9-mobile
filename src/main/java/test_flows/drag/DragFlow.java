ee6b08953495c48d06caeccc60957a28daf08102
vinhdothanhpg <vinh.dothanh@powergatesoftware.co>
2022-10-11 09:34:50 +0700

    update dependency

---
+++ src/main/java/test_flows/drag/DragFlow.java
@@ -0,0 +1,14 @@
+package test_flows.drag;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import test_flows.BaseFlow;
+
+public class DragFlow extends BaseFlow {
+
+    public DragFlow(AppiumDriver<MobileElement> appiumDriver) {
+        super(appiumDriver);
+    }
+
+
+}
